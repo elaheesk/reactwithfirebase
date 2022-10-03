9b4a55872bcf23c4059d815f152cea12dce89544
elaheeskandari <elahe199108@gmail.com>
2022-10-03 18:28:23 +0200

    removed the background image fixed some styling

--- src/App.js
+++ src/App.js
@@ -9,181 +9,179 @@ import { Grid, Typography } from "@mui/material";
 
 import { db } from "./firebase-config";
 import {
-  collection,
-  getDocs,
-  addDoc,
-  updateDoc,
-  doc,
-  deleteDoc,
+	collection,
+	getDocs,
+	addDoc,
+	updateDoc,
+	doc,
+	deleteDoc,
 } from "firebase/firestore";
 function App() {
-  const [users, setUsers] = React.useState([]);
-  const [likeUser, setLikeUser] = React.useState([]);
-  const [selectedPerson, toggleInputs] = React.useState(-1);
-  const [allInputVals, setAllInputVals] = React.useState({
-    id: "",
-    name: "",
-    age: "",
-    quote: "",
-  });
+	const [users, setUsers] = React.useState([]);
+	const [likeUser, setLikeUser] = React.useState([]);
+	const [selectedPerson, toggleInputs] = React.useState(-1);
+	const [allInputVals, setAllInputVals] = React.useState({
+		id: "",
+		name: "",
+		age: "",
+		quote: "",
+	});
 
-  const [inputValsToUpdate, setInputValsToUpdate] = React.useState({
-    id: "",
-    name: "",
-    age: 0,
-    quote: "",
-  });
+	const [inputValsToUpdate, setInputValsToUpdate] = React.useState({
+		id: "",
+		name: "",
+		age: 0,
+		quote: "",
+	});
 
-  const usersCollectionRef = collection(db, "users");
+	const usersCollectionRef = collection(db, "users");
 
-  const handleChange = (evt) => {
-    setAllInputVals({
-      ...allInputVals,
-      [evt.target.name]:
-        evt.target.value.charAt(0).toUpperCase() + evt.target.value.slice(1),
-      [evt.target.age]: evt.target.value,
-      [evt.target.quote]: evt.target.value,
-    });
-  };
+	const handleChange = (evt) => {
+		setAllInputVals({
+			...allInputVals,
+			[evt.target.name]:
+				evt.target.value.charAt(0).toUpperCase() + evt.target.value.slice(1),
+			[evt.target.age]: evt.target.value,
+			[evt.target.quote]: evt.target.value,
+		});
+	};
 
-  const openSelected = (user, index) => {
-    toggleInputs(selectedPerson === index ? -1 : index);
+	const openSelected = (user, index) => {
+		toggleInputs(selectedPerson === index ? -1 : index);
 
-    const copyUsersArray = [...users];
-    copyUsersArray[index] = {
-      id: user.id,
-      name: user.name,
-      age: user.age,
-      quote: user.quote,
-      liked: user.liked,
-    };
-    setUsers(copyUsersArray);
+		const copyUsersArray = [...users];
+		copyUsersArray[index] = {
+			id: user.id,
+			name: user.name,
+			age: user.age,
+			quote: user.quote,
+			liked: user.liked,
+		};
+		setUsers(copyUsersArray);
 
-    setInputValsToUpdate({ name: user.name, age: user.age, quote: user.quote });
-  };
-  const createUser = async (allInputVals) => {
-    await addDoc(usersCollectionRef, {
-      name: allInputVals.name,
-      age: allInputVals.age,
-      quote: allInputVals.quote,
-      liked: false,
-    });
-    const data = await getDocs(usersCollectionRef);
-    setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    setAllInputVals({ name: "", age: "", quote: "" });
-    // console.log("nyskapade users", users);
-  };
+		setInputValsToUpdate({ name: user.name, age: user.age, quote: user.quote });
+	};
+	const createUser = async (allInputVals) => {
+		await addDoc(usersCollectionRef, {
+			name: allInputVals.name,
+			age: allInputVals.age,
+			quote: allInputVals.quote,
+			liked: false,
+		});
+		const data = await getDocs(usersCollectionRef);
+		setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		setAllInputVals({ name: "", age: "", quote: "" });
+	};
 
-  const updateUser = async (id) => {
-    const userDoc = doc(db, "users", id);
-    const newFields = {
-      id: id,
-      age: inputValsToUpdate.age,
-      name: inputValsToUpdate.name,
-      quote: inputValsToUpdate.quote,
-    };
-    await updateDoc(userDoc, newFields);
-    const data = await getDocs(usersCollectionRef);
-    console.log("vilket id", newFields.id);
-    setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    console.log("Elaheeeee", users);
-    setInputValsToUpdate({ name: "", age: "", quote: "" });
-    openSelected();
-  };
+	const updateUser = async (id) => {
+		const userDoc = doc(db, "users", id);
+		const newFields = {
+			id: id,
+			age: inputValsToUpdate.age,
+			name: inputValsToUpdate.name,
+			quote: inputValsToUpdate.quote,
+		};
+		await updateDoc(userDoc, newFields);
+		const data = await getDocs(usersCollectionRef);
+		console.log("vilket id", newFields.id);
+		setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		console.log("Elaheeeee", users);
+		setInputValsToUpdate({ name: "", age: "", quote: "" });
+		openSelected();
+	};
 
-  const deleteUser = async (id) => {
-    const userDoc = doc(db, "users", id);
-    await deleteDoc(userDoc);
-    const data = await getDocs(usersCollectionRef);
-    setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  };
+	const deleteUser = async (id) => {
+		const userDoc = doc(db, "users", id);
+		await deleteDoc(userDoc);
+		const data = await getDocs(usersCollectionRef);
+		setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+	};
 
-  React.useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getUsers();
-  }, []);
+	const changeLikeProp = (likedUser) => {
+		if (users.length !== 0) {
+			const newArr = users.map((user) => {
+				if (user.id === likedUser.id) {
+					return { ...likedUser, liked: !user.liked };
+				} else {
+					return user;
+				}
+			});
+			setUsers(newArr);
+		}
+	};
 
-  React.useEffect(() => {
-    if (users.length !== 0) {
-      const myLikedUsers = users.filter((user) => {
-        if (user.liked === true) {
-          return user;
-        }
-      });
-      setLikeUser(myLikedUsers);
-    }
-  }, [users]);
+	React.useEffect(() => {
+		const getUsers = async () => {
+			const data = await getDocs(usersCollectionRef);
+			setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+		getUsers();
+	}, []);
 
-  const changeLikeProp = (likedUser) => {
-    if (users.length !== 0) {
-      const newArr = users.map((user) => {
-        if (user.id === likedUser.id) {
-          return { ...likedUser, liked: !user.liked };
-        } else {
-          return user;
-        }
-      });
-      setUsers(newArr);
-    }
-  };
+	React.useEffect(() => {
+		if (users.length !== 0) {
+			const myLikedUsers = users.filter((user) => {
+				if (user.liked === true) {
+					return user;
+				}
+			});
+			setLikeUser(myLikedUsers);
+		}
+	}, [users]);
 
-  return (
-    <Grid className="PictureGrid" container>
-      <Grid container justifyContent="center">
-        <Grid item>
-          <Typography
-            sx={{ fontFamily: "cursive", color: "bisque" }}
-            variant="h4"
-            gutterBottom
-          >
-            Elahes Quote List
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <NavigationBar likeUser={likeUser} />
-      </Grid>
-      <Grid item>
-        <Routes>
-          <Route
-            path="mylikes"
-            element={
-              <MyLikes
-                likeUser={likeUser}
-                setLikeUser={setLikeUser}
-                changeLikeProp={changeLikeProp}
-              />
-            }
-          />
+	return (
+		<Grid className="PictureGrid" container>
+			<Grid container justifyContent="center">
+				<Grid item>
+					<Typography
+						sx={{ fontFamily: "cursive", color: "bisque" }}
+						variant="h4"
+						gutterBottom>
+						Elahes Quote List
+					</Typography>
+				</Grid>
+			</Grid>
+			<Grid item>
+				<NavigationBar likeUser={likeUser} />
+			</Grid>
+			<Grid item>
+				<Routes>
+					<Route
+						path="mylikes"
+						element={
+							<MyLikes
+								likeUser={likeUser}
+								setLikeUser={setLikeUser}
+								changeLikeProp={changeLikeProp}
+							/>
+						}
+					/>
 
-          <Route
-            path="/"
-            element={
-              <Home
-                users={users}
-                setUsers={setUsers}
-                likeUser={likeUser}
-                setLikeUser={setLikeUser}
-                selectedPerson={selectedPerson}
-                openSelected={openSelected}
-                inputValsToUpdate={inputValsToUpdate}
-                setInputValsToUpdate={setInputValsToUpdate}
-                createUser={createUser}
-                updateUser={updateUser}
-                deleteUser={deleteUser}
-                changeLikeProp={changeLikeProp}
-                allInputVals={allInputVals}
-                handleChange={handleChange}
-              />
-            }
-          />
-        </Routes>
-      </Grid>
-    </Grid>
-  );
+					<Route
+						path="/"
+						element={
+							<Home
+								users={users}
+								setUsers={setUsers}
+								likeUser={likeUser}
+								setLikeUser={setLikeUser}
+								selectedPerson={selectedPerson}
+								openSelected={openSelected}
+								inputValsToUpdate={inputValsToUpdate}
+								setInputValsToUpdate={setInputValsToUpdate}
+								createUser={createUser}
+								updateUser={updateUser}
+								deleteUser={deleteUser}
+								changeLikeProp={changeLikeProp}
+								allInputVals={allInputVals}
+								handleChange={handleChange}
+							/>
+						}
+					/>
+				</Routes>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default App;
--- src/components/ListLayout.js
+++ src/components/ListLayout.js
@@ -8,169 +8,164 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import { styled } from "@mui/styles";
 import { Button, Grid, List, TextField, Typography } from "@mui/material";
 const ListLayout = ({
-  user,
-  index,
-  selectedPerson,
-  openSelected,
-  inputValsToUpdate,
-  setInputValsToUpdate,
-  updateUser,
-  deleteUser,
-  changeLikeProp,
+	user,
+	index,
+	selectedPerson,
+	openSelected,
+	inputValsToUpdate,
+	setInputValsToUpdate,
+	updateUser,
+	deleteUser,
+	changeLikeProp,
 }) => {
-  const handleChangeNewInputs = (evt) => {
-    setInputValsToUpdate({
-      ...inputValsToUpdate,
-      [evt.target.name]:
-        evt.target.value.charAt(0).toUpperCase() + evt.target.value.slice(1),
-      [evt.target.age]: evt.target.value,
-      [evt.target.quote]: evt.target.value,
-    });
-  };
+	const handleChangeNewInputs = (evt) => {
+		setInputValsToUpdate({
+			...inputValsToUpdate,
+			[evt.target.name]:
+				evt.target.value.charAt(0).toUpperCase() + evt.target.value.slice(1),
+			[evt.target.age]: evt.target.value,
+			[evt.target.quote]: evt.target.value,
+		});
+	};
 
-  const MyHeading = styled(Grid)({
-    border: 0,
-    height: 32,
-    borderRadius: 5,
-    background: "linear-gradient(45deg, bisque 30%, rgb(136, 223, 214)90%)",
-    boxShadow: "0 2px 4px 2px rgba(30, 199, 230, .5)",
-    color: "white",
-  });
-  return (
-    <Grid item key={`item-${index}`} sx={{ width: "600px" }}>
-      <List
-        className={`item ${selectedPerson === index ? "open" : ""}`}
-        sx={{
-          boxShadow: "0px 1px 10px 1px  black inset",
-          marginBottom: "10px",
-          padding: 2,
-          borderRadius: 5,
-        }}
-      >
-        <Grid container item justifyContent="space-between">
-          <Grid item>
-            {" "}
-            <Typography
-              component="div"
-              variant="h6"
-              sx={{
-                color: "white",
-                textShadow: "1px 1px black",
-              }}
-            >
-              {user.name}, {user.age} years
-            </Typography>
-            <Typography
-              variant="body1"
-              component="div"
-              sx={{
-                color: "darkslategray",
-              }}
-            >
-              {" "}
-              Quote: {user.quote}
-            </Typography>
-          </Grid>
-          <Grid item>
-            <MyHeading>
-              <Button
-                className="editButton"
-                onClick={() => openSelected(user, index)}
-                startIcon={<EditIcon />}
-              >
-                Edit
-              </Button>
-            </MyHeading>
-          </Grid>
-        </Grid>
-        <Grid sx={{ marginTop: "20px" }} className="editTextFields">
-          <TextField
-            value={inputValsToUpdate.name}
-            name="name"
-            label="Type name"
-            onChange={handleChangeNewInputs}
-            size="small"
-            sx={{
-              marginRight: "10px",
-              width: 150,
-              boxShadow: "0px 1px 10px 1px  black inset",
-              borderRadius: 1,
-            }}
-          />
-          <TextField
-            value={inputValsToUpdate.age}
-            name="age"
-            label="Type age"
-            type="number"
-            placeholder=" age"
-            onChange={handleChangeNewInputs}
-            size="small"
-            sx={{
-              marginRight: "10px",
-              width: 150,
-              boxShadow: "0px 1px 10px 1px  black inset",
-              borderRadius: 1,
-            }}
-          />
+	const MyHeading = styled(Grid)({
+		border: 0,
+		height: 32,
+		borderRadius: 5,
+		background: "linear-gradient(45deg, bisque 30%, rgb(136, 223, 214)90%)",
+		boxShadow: "0 2px 4px 2px rgba(30, 199, 230, .5)",
+		color: "white",
+	});
+	return (
+		<Grid item key={`item-${index}`}
+    >
+			<List
+				className={`item ${selectedPerson === index ? "open" : ""}`}
+				sx={{
+					boxShadow: "0px 1px 10px 1px  black inset",
+					marginBottom: "10px",
+					padding: 2,
+					borderRadius: 5,
+				}}>
+				<Grid container item justifyContent="space-between">
+					<Grid item>
+						{" "}
+						<Typography
+							component="div"
+							variant="h6"
+							sx={{
+								color: "brown",
+								textShadow: "1px 1px black",
+							}}>
+							{user.name}, {user.age} years
+						</Typography>
+						<Typography
+							variant="body1"
+							component="div"
+							sx={{
+								color: "darkslategray",
+							}}>
+							{" "}
+							Quote: {user.quote}
+						</Typography>
+					</Grid>
+					<Grid item>
+						<MyHeading>
+							<Button
+								className="editButton"
+								onClick={() => openSelected(user, index)}
+								startIcon={<EditIcon />}>
+								Edit
+							</Button>
+						</MyHeading>
+					</Grid>
+				</Grid>
+				<Grid sx={{ marginTop: "20px" }} className="editTextFields">
+					<TextField
+						value={inputValsToUpdate.name}
+						name="name"
+						label="Type name"
+						onChange={handleChangeNewInputs}
+						size="small"
+						sx={{
+							marginRight: "10px",
+							width: 150,
+							boxShadow: "0px 1px 10px 1px  black inset",
+							borderRadius: 1,
+						}}
+					/>
+					<TextField
+						value={inputValsToUpdate.age}
+						name="age"
+						label="Type age"
+						type="number"
+						placeholder=" age"
+						onChange={handleChangeNewInputs}
+						size="small"
+						sx={{
+							marginRight: "10px",
+							width: 150,
+							boxShadow: "0px 1px 10px 1px  black inset",
+							borderRadius: 1,
+						}}
+					/>
 
-          <TextField
-            sx={{ boxShadow: "0px 1px 10px 1px  black inset", borderRadius: 1 }}
-            id="outlined-multiline-static"
-            label="Type quote"
-            multiline
-            rows={3}
-            value={inputValsToUpdate.quote}
-            name="quote"
-            onChange={handleChangeNewInputs}
-          />
+					<TextField
+						sx={{ boxShadow: "0px 1px 10px 1px  black inset", borderRadius: 1 }}
+						id="outlined-multiline-static"
+						label="Type quote"
+						multiline
+						rows={3}
+						value={inputValsToUpdate.quote}
+						name="quote"
+						onChange={handleChangeNewInputs}
+					/>
 
-          <Button
-            variant="outlined"
-            sx={{
-              background:
-                "linear-gradient(45deg, bisque 30%, rgb(136, 223, 214)90%)",
-              boxShadow: "0 2px 4px 2px rgba(30, 199, 230, .5)",
-            }}
-            size="small"
-            startIcon={<DoneIcon />}
-            onClick={() => {
-              updateUser(user.id);
-            }}
-          >
-            update
-          </Button>
-        </Grid>{" "}
-        <Grid item container justifyContent="space-between">
-          <Grid item>
-            <Button
-              size="small"
-              sx={{
-                marginTop: "10px",
-                background:
-                  "linear-gradient(45deg, rgb(107, 104, 104) 30%, #bcaaa4 90%)",
-                boxShadow: "0 2px 4px 2px rgba(30, 199, 230, .3)",
-                borderRadius: 1,
-              }}
-              startIcon={<DeleteIcon sx={{ color: "white" }} />}
-              onClick={() => {
-                deleteUser(user.id);
-              }}
-            >
-              Delete
-            </Button>
-          </Grid>
-          <Grid item>
-            <IconButton onClick={() => changeLikeProp(user)}>
-              {user.liked ? (
-                <FavoriteIcon style={{ fill: "red" }} />
-              ) : (
-                <FavoriteIcon style={{ fill: "gray" }} />
-              )}
-            </IconButton>
-          </Grid>
-        </Grid>
-      </List>
-    </Grid>
-  );
+					<Button
+						variant="outlined"
+						sx={{
+							background:
+								"linear-gradient(45deg, bisque 30%, rgb(136, 223, 214)90%)",
+							boxShadow: "0 2px 4px 2px rgba(30, 199, 230, .5)",
+						}}
+						size="small"
+						startIcon={<DoneIcon />}
+						onClick={() => {
+							updateUser(user.id);
+						}}>
+						update
+					</Button>
+				</Grid>{" "}
+				<Grid item container justifyContent="space-between">
+					<Grid item>
+						<Button
+							size="small"
+							sx={{
+								marginTop: "10px",
+								background:
+									"linear-gradient(45deg, rgb(107, 104, 104) 30%, #bcaaa4 90%)",
+								boxShadow: "0 2px 4px 2px rgba(30, 199, 230, .3)",
+								borderRadius: 1,
+							}}
+							startIcon={<DeleteIcon sx={{ color: "white" }} />}
+							onClick={() => {
+								deleteUser(user.id);
+							}}>
+							Delete
+						</Button>
+					</Grid>
+					<Grid item>
+						<IconButton onClick={() => changeLikeProp(user)}>
+							{user.liked ? (
+								<FavoriteIcon style={{ fill: "red" }} />
+							) : (
+								<FavoriteIcon style={{ fill: "gray" }} />
+							)}
+						</IconButton>
+					</Grid>
+				</Grid>
+			</List>
+		</Grid>
+	);
 };
 export default ListLayout;
